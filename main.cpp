46fd36b83cba8bbf3d3359dcf087bafc6ef529e3
edKotinsky <edkotinsky@gmail.com>
2022-12-15 08:58:43 +0600

    Added tests

--- main.cpp
+++ main.cpp
@@ -18,10 +18,14 @@
 #include <iostream>
 #include <sstream>
 #include <cassert>
+#include <chrono>
+#include <thread>
 
 int main() {
 	uniprint::printer<std::cout> print;
 	using namespace uniprint::options;
+
+	print("Hello from test!");
 
 	// test 1
 	{
@@ -45,6 +49,30 @@ int main() {
 
 		print(file{ostream}, "Hello", sep{"_"}, "World", end{"!"});
 		assert(ostream.str() == "Hello_World!");
+	}
+
+	// test 4
+	{
+		std::ostringstream ostream;
+
+		print(1, 2, 3, "Integer sequence", 
+				1.41, 2.71, 3.14, "Floating point sequence", file{ostream});
+		assert(ostream.str() ==
+				"1 2 3 Integer sequence 1.41 2.71 3.14 Floating point sequence\n");
+	}
+
+	// test 5
+	{
+		for (int i = 1; i < 11; ++i) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(300));
+			print(i, end{""}, flush{});
+			for (int j = 0; j < 3; ++j) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				print('.', end{""}, flush{});
+			}
+		}
+		print();
+		print("All tests are passed!");
 	}
 
 	return 0;
