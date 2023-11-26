16f7f32960af591de0a7f56735ad2bf364e646ae
glebzlat <edkotinsky@gmail.com>
2023-11-26 13:46:55 +0600

    Shorten the example

--- examples/uniprint.cpp
+++ examples/uniprint.cpp
@@ -1,41 +1,13 @@
 #include "uniprint.hpp"
-#include <cassert>
 #include <iostream>
-#include <sstream>
 
 int main() {
   uniprint::print print(std::cout);
-
   using namespace uniprint::args;
 
-  print("Hello", "world!");
-  print(sep{"_"}, "Underscores", "instead", "of", "spaces");
-  print(end{"!"}, "Hello", "world", "again");
-  print(" WOW!", "No EOF");
-
-  print();
-
-  print(3, sep{"."}, 14, end{" - "});
-  print("This is PI", end{""});
-  print(end{"!"});
-
-  print();
-
-  {
-    std::ostringstream s;
-
-    print(file{s}, "Hello", "World", '!');
-
-    assert(s.str() == std::string{"Hello World !\n"});
-  }
-
-  {
-    std::ostringstream s;
-
-    print(file{s}, "Hello", "World", end{"!"});
-
-    assert(s.str() == std::string{"Hello World!"});
-  }
+  print("Hello", "world", sep{", "}, end{"!\n"});
+  print(1, 2, 3, sep{"\n"});
+  print(file{std::cerr}, "Message"); // yep, kwarg before positional
 
   return 0;
 }
